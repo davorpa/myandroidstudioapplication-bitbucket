0393aabff2c31434e1031ef209e583424c9e172c
davorpa <katelme@gmail.com>
2020-03-04 15:12:24 +0100

    Add/apply ButterKnife library

--- app/src/main/java/com/davorpa/labs/androidstudio/bitbucket/MainActivity.java
+++ app/src/main/java/com/davorpa/labs/androidstudio/bitbucket/MainActivity.java
@@ -3,12 +3,36 @@ package com.davorpa.labs.androidstudio.bitbucket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.TextView)
+    TextView textView;
+
+    @BindString(R.string.hello_world)
+    String helloWorldText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Capture the layout's components, resources... handled by ButterKnife annotations
+        ButterKnife.bind(this);
+
+        // configure each view component
+        configureTextView(savedInstanceState);
     }
+
+
+    protected void configureTextView(final Bundle savedInstanceState) {
+        textView.setText(helloWorldText);
+    }
+
 }
